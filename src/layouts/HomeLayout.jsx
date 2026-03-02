48660383aa39cff5d0fb91369b64bb4b171e876e
jubaeddddd <jubaed.techie@gmail.com>
2026-03-03 04:17:24 +0600

    first commit

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <h1>Home Layout....</h1>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ src/routes/router.jsx
@@ -0,0 +1,11 @@
+import {
+  createBrowserRouter
+} from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout></HomeLayout>,
+  },
+]);
